4b01ae8ea136f7a9b67922426aaa0ccb209bd80c
Marco Postman <89901767+calenorpobil@users.noreply.github.com>
2025-01-09 18:21:41 +0100

    First commit

---
+++ Builds/Android/app/build/intermediates/cxx/Debug/6c5k1o1n/logs/x86_64/build_command_juce_jni.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Carlos\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Carlos\\Documents\\Merlita\\Estudio\\Informatica\\JUCE\\Juan\\Builds\\Android\\app\\.cxx\\Debug\\6c5k1o1n\\x86_64" ^
+  juce_jni
